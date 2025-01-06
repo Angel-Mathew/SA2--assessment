1dcdd49f8ea5e9070b374e0014a2b12609d6352e
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2025-01-06 11:39:38 +0400

    updated

--- main page/main page.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("account_form").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("account_form").style.display = "none";
-  }
